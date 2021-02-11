446291c5b879002dc73f477d03637cc1a33a6c92
ItoFumitsugu <>
2021-02-11 10:15:10 +0900

    stateにmodelとなるデータを作成。 チャットの元になるデータをdataset.jsとしてファイルを新規追加

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import './App.css';
+import defaultDataset from "./dataset";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      answers: [],
+      chats: [],
+      currentID: 'init',
+      dataset: defaultDataset,
+      open: false
+    }
+  }
+
   render() {
     return (
       <div>
