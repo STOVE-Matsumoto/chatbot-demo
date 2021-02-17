71e50f4665ed5e425b6a502441b44de69b98fe75
ItoFumitsugu <>
2021-02-18 00:09:42 +0900

    Answer Componentの概略を作成

--- src/components/AnswersList.jsx
+++ src/components/AnswersList.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import {Answer} from './index'
 
 const AnswersList = () => {
   return(
     <div className="c-grid__answer">
-
+      <Answer />
+      <Answer />
+      <Answer />
+      <Answer />
     </div>
-  )
+  );
 }
 
-export default AnswersList
+export default AnswersList;
